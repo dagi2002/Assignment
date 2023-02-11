1824a469f0d4830d55c76ecf35d9d8117aefb4b5
dagi2002 <Dagemamogne@gmail.com>
2023-02-11 15:47:05 +0300

    error fix

--- german_restaurant/lib/main.dart
+++ german_restaurant/lib/main.dart
@@ -13,52 +13,107 @@ class MyApp extends StatelessWidget {
 
     final ThemeData theme = ThemeData();
     return MaterialApp(
-      title: 'German Restaurant',
+
+        title: 'German Restaurant',
         theme: theme.copyWith(
           colorScheme: theme.colorScheme.copyWith(
             primary: Colors.grey,
             secondary: Colors.black,
           ),
         ),
-      home: const MyHomePage(title: 'German Restaurant'),
-    );
+      home: categoriesCard());
+
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class categoriesCard extends StatelessWidget {
+  const categoriesCard({super.key});
 
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-        ),
-        body: SafeArea(
-          child: ListView.builder(
+      backgroundColor: Colors.white12,
+      appBar: AppBar(
+        title: const Text("Menu - Categories",
+        style: TextStyle(
+          color: Colors.black54,
+        )),
+        backgroundColor: Colors.white12,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: ListView(
+          children: [
+            Card(
+              surfaceTintColor: Colors.white12,
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child:  Column(
+                children:  [
+                  Image.network("https://images.squarespace-cdn.com/content/v1/59fc723aa8b2b08256276cb1/1585547242207-781KACCVVC299TILFMZM/German+Dumplings+–+Knödeln%C2%A0.jpg?format=1000w"),
+                  const Text("Main dishes",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    backgroundColor: Colors.white12
+                  ),)
+                ],
 
-            itemBuilder: (context ,index){
-                return categoriesCard();
-            }
-          ),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child:  Column(
+                children:  [
+                  Image.network("https://www.billyparisi.com/wp-content/uploads/2021/08/fries-1.jpg"),
+                  const Text("Side dishes",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                    ),)
+                ],
+
+              ),
+            ),
+            Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child:  Column(
+                children:  [
+                  Image.network("https://assets3.thrillist.com/v1/image/1693021/828x610/flatten;crop;webp=auto;jpeg_quality=60.jpg"),
+                  const Text("Drinks",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                    ),)
+                ],
+
+              ),
+            ),
+          ],
         ),
+
+
+
+
+
+
+
+          ),
+
+
+
+
+
+
 
     );
-
 }
-
-
 
 
 }
